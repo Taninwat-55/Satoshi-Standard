fb729f5a1153b9f67a606ee05fcaf5445412bcb7
Taninwat-55 <60184555+Taninwat-55@users.noreply.github.com>
2026-01-06 04:30:37 +0100

    update sats only mode

--- src/components/layout/SatsToggle.jsx
+++ src/components/layout/SatsToggle.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSavedItems } from '../../contexts/SavedItemsContext';
+import { useSavedItems } from '../../hooks/useSavedItems';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 export default function SatsToggle() {
